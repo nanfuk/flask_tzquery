6f48e64e091c814d9d223946cebab938ca73ebe9
runner56 <759775069@qq.com>
2015-09-28 09:27:54 +0800

    结构化调整

--- test/test.py
+++ test/test.py
@@ -1,13 +1,14 @@
 # -*- coding:utf8 -*-
-import os
-#os.system("dir")
-import subprocess
-import shutil
+from flask import Flask
 
-#filePath = r"\\10.117.193.234\资源调度\传输\SDH客户路由台账\客户电路1(融合资源).xlsx".decode('utf8')
-filePath = r"C:\传输值班台账汇总1\3G基站电路台帐(最新).xlsm".decode("utf8")
-excelDir = r"C:\传输值班台账汇总".decode('utf8')
-try:
-    shutil.copy(filePath, excelDir)
-except Exception,e:
-    print e
+
+
+app = Flask(__name__)
+
+@app.before_first_request
+def test():
+    print "test"
+
+if __name__ == "__main__":
+    #test()
+    app.run(debug=True)
