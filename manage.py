6560aa67946eff0e20b219031f45958f2cc9e059
runner56 <759775069@qq.com>
2015-12-12 15:41:45 +0800

    加入了fav.icon; 修正了波分端口表，加入槽位号等信息; 加入了通过manage.py runserver 0.0.0.0来运行服务。

--- manage.py
+++ manage.py
@@ -3,7 +3,9 @@
 from flask.ext.script import Manager	#安装Flask-Script
 from flask_tzquery.api import create_app
 
+app = create_app()
 manager = Manager(create_app())
 
+
 if __name__ == '__main__':
-	manager.run()
+	manager.run()	#怎么改host为0.0.0.0，python manage.py runserver -h 0.0.0.0
